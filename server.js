80f332174660661a60d9ca383447c8a6e84e7917
Jason <jasonpwn1@gmail.com>
2019-02-16 17:51:48 -0500

    hellow

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 // 7. Add graphing scale functionality
 
 
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 8080;
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
